686aacb9e1d8d4ac2eac34f7f91c4e09499cdc46
M4theuszzOJR <Matheusmcpp@gmail.com>
2025-03-07 21:52:46 -0300

    Update Cod.js

--- Cod.js
+++ Cod.js
@@ -9,3 +9,20 @@ for (var i = 2; i <= n; i++ ){
 alert("O número na posição" + n + "da sequência de Fibonacci é:" + (n == 0 ? 0: b));
 var matheus = prompt("Digite algo: ")
 alert(matheus)
+
+
+
+var nome = prompt ("Digite seu nome")
+var numeroconta = prompt ("Digite o número da conta")
+var saldocliente = 1000
+
+function depositar () {
+    var valor = prompt ("Digite o valor que você deseja depositar")
+    if ( valor <= 1000 ) {
+      saldocliente = saldocliente + valor
+      alert ("O valor foi depositado à sua conta, seu novo saldo é " + saldocliente)  
+    }
+    else {
+        alert ("Erro! O valor escolhido é maior que 1000.")
+    }
+}
